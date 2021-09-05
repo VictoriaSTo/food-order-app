7f73de4ba5b9690b38ac4d2ad421e7227dc9b52b
VictoriaSTo <78522147+VictoriaSTo@users.noreply.github.com>
2021-09-05 21:30:28 +0800

    added meals and card components

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealItemForm = () => {
+  return (<div>
+    <form>
+      <input type="number"></input>
+    </form>
+    <button>+ Add</button>
+  </div>)
+};
+
+export default MealItemForm;
